3b432afc75f2b6ceff0270e66c552b5227b4caf0
Vivek P <vivekpatil2092@gmail.com>
2015-10-27 02:01:34 +0530

    Integrate lein midge for test suits

--- project.clj
+++ project.clj
@@ -2,4 +2,6 @@
   :description "First-class patterns for Clojure"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dev-dependencies [[midje "1.4.0"]
+                     [lein-midje "1.0.10"]]
   :dependencies [[org.clojure/clojure "1.6.0"]])
